05eeccad1b9a235afcd5e70342c8ff2ceec62d2b
minhajkhan75 <minhajuddinkhan75@gmail.com>
2018-08-05 12:43:38 +0500

    read csv

--- utils/csv.go
+++ utils/csv.go
@@ -2,7 +2,11 @@ package utils
 
 import (
 	"encoding/csv"
+	"io"
+	"io/ioutil"
+	"log"
 	"os"
+	"strings"
 )
 
 //WriteCsv WriteCsv
@@ -24,4 +28,26 @@ func WriteCsv(dir string, filename string, records [][]string) error {
 	csvWriter.Flush()
 	return nil
 
+}
+
+//ReadCsv ReadCsv
+func ReadCsv(dir, filename string) [][]string {
+
+	b, _ := ioutil.ReadFile(dir + filename)
+	r := csv.NewReader(strings.NewReader(string(b)))
+
+	var records [][]string
+
+	for {
+		record, err := r.Read()
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			log.Fatal(err)
+		}
+		records = append(records, record)
+	}
+
+	return records
 }
